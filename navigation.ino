bfb751484839efaffe7c6c2cde6da85e095f83b5
wkemilylau <123195547+wkemilylau@users.noreply.github.com>
2024-01-30 11:28:22 +0000

    Add files via upload

--- navigation.ino
+++ navigation.ino
@@ -25,7 +25,7 @@ int valSideRight = digitalRead(lineSideRightPin); // read side right input value
 // finger covering line sensor, line sensor lighting up means a reading of 1
 
 // array of routes; does not include backing out in free space; does not include U-turns after picking block up in line area
-String routes[16] = {           
+char routes[16][6] = {           
           "RL",               // 0: block 1 to green
           "LSR",              // 1: block 1 to red
           "SRSR",             // 2: green to block 2
@@ -44,7 +44,7 @@ String routes[16] = {
           "LL"};              // 15: red to finish
 
 // number of junctions of each route, used when passing as second argument to routefollow()
-int sizeOfRoutes[16] = {3,4,5,4,5,4,4,6,6,3,4,4,4,5,4,3};
+int sizeOfRoutes[16] = {2,3,4,3,4,3,3,5,5,2,3,3,3,4,3,2};
 
 // Set speed constants
 const int HighSpeed = 150;            // adjustment on straight line
@@ -98,25 +98,28 @@ void updateleftmotorspeed(int NewLeftMotorSpeed) {    // update left motor speed
   }
 }
 
-void junctionrotation(String Direction) {    // C++ requires 2 spaces to store 1 character
+void junctionrotation(char Direction[2]) {    // C++ requires 2 spaces to store 1 character
                                               // Direction == "R" means turn right, "L" means left, "S" (or other char) means straight line
     Serial.println("start to rotate");
     
     updateleftmotorspeed(RotationSpeed);      // set constant rotate speed
     updaterightmotorspeed(RotationSpeed);
     
-    if (Direction == "R") {
+    if (Direction == 'R') {
       motor_left->run(FORWARD);
       motor_right->run(BACKWARD);
-    } else if (Direction == "L") {
+    } else if (Direction == 'L') {
       motor_left->run(BACKWARD);
       motor_right->run(FORWARD);
-    } else if (Direction == "S") {
+    } else if (Direction == 'S') {
       motor_left->run(FORWARD);
       motor_right->run(FORWARD);
       while (valSideRight == 1) {
         gostraight();
       }
+      return;
+    } else if (Direction == '\0'){    // reaches the end (null terminator) of the input character // NOT SURE IF ITS CORRECT
+      stopmoving();       
       return;
     } else {
       Serial.println("BAD INPUT");
@@ -196,7 +199,7 @@ void gostraight() {   // walk in straight line
   delay(10);        // we need delay for the motor to respond
 }
 
-void routefollow(String route[], int numberOfJunctions) {
+void routefollow(const char route[], int numberOfJunctions) {
   
   for (int currentJunction = 0; currentJunction < numberOfJunctions; currentJunction++) {
     
@@ -230,15 +233,14 @@ void loop() {
   // int blockNumber = 0;
   // int notPickup = -1;
   // int blockType = -1;
-  routefollow(String("LR"), 2);
-  junctionrotation(String('R'));
-  for (int routePtr = 0; routePtr < 4; routePtr = routePtr + 2) {
+  // routefollow("LR", 2);
+  // junctionrotation('R');
+  for (int routePtr = 0; routePtr < 16; routePtr = routePtr + 2) {
     //int routePtr = (blockNumber - 1) * 4 + notPickup * 2 + blockType;
-    int numberOfJunctions = routes[routePtr].length();
+    int numberOfJunctions = sizeOfRoutes[routePtr];
     routefollow(routes[routePtr], numberOfJunctions);
-    junctionrotation(String('R'));
+    junctionrotation('R');
   }
 
   delay(100000);
 }
-
