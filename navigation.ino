155b713943c7d985fbd8e988c34bf5395e76f33b
wkemilylau <123195547+wkemilylau@users.noreply.github.com>
2024-02-05 14:18:15 +0000

    Add files via upload

--- navigation.ino
+++ navigation.ino
@@ -394,45 +394,55 @@ void identifyblock() {
 
 void loop() {
   // // in C++, size of char = number of letters + 1
-  // routePtr = (blockNumber - 1) * 4 + !pickup * 2 + blockType;
+  // start
+
+  routePtr = (blockNumber - 1) * 4 + !pickup * 2 + blockType;
 
   // Serial.println("-------------------------");
   // Serial.println(blockNumber);
   // Serial.println(pickup);
   // Serial.println(routePtr);
 
-  // if (routePtr < 0) {                   // from start to block 1
-  //   routefollow("LR", 2);
-  //   identifyblock();
-  //   liftblock();
-  // } else if (routePtr >= 14) {          // return to finish
-  //   junctionrotation('R');
-  //   int numberOfJunctions = sizeOfRoutes[routePtr];
-  //   routefollow(routes[routePtr], numberOfJunctions);
-  //   while (1) {                         // stop after finish
-  //     stopmoving();
-  //   }    
-  // } else {
-  //   if (!pickup || blockNumber == 1 || blockNumber == 2) {           // u turn to leave outpost and residential area
-  //     junctionrotation('R');                                         
-  //   }
-  //   int numberOfJunctions = sizeOfRoutes[routePtr];
-  //   routefollow(routes[routePtr], numberOfJunctions);
+  if (routePtr < 0) {                   // from start to block 1
 
-  //   if ((blockNumber == 2 || blockNumber == 3) && !pickup) {         // arrived open area
-  //     findandapproachblock(); 
-  //     identifyblock();
-  //     liftblock();
-  //     returntoline();
-  //   } else if (pickup) {                                             // arrived outpost
-  //     release();
-  //   } else if ((blockNumber == 0 || blockNumber == 1) && !pickup) {  // arrived residential area                                                         
-  //     identifyblock();
-  //     liftblock();
-  //   }
-  // }
+    updatelinesensors();
+    while (valLeft == 0 || valRight == 0) {   // leave start block
+      gostraight();
+      delay(5);
+      updatelinesensors();
+    }
 
-  while (1) {
-    stopmoving();
+    routefollow("SLR", 3);
+    identifyblock();
+    liftblock();
+  } else if (routePtr >= 14) {          // return to finish
+    junctionrotation('R');
+    int numberOfJunctions = sizeOfRoutes[routePtr];
+    routefollow(routes[routePtr], numberOfJunctions);
+    while (1) {                         // stop after finish
+      stopmoving();
+    }    
+  } else {
+    if (!pickup || blockNumber == 1 || blockNumber == 2) {           // u turn to leave outpost and residential area
+      junctionrotation('R');                                         
+    }
+    int numberOfJunctions = sizeOfRoutes[routePtr];
+    routefollow(routes[routePtr], numberOfJunctions);
+
+    if ((blockNumber == 2 || blockNumber == 3) && !pickup) {         // arrived open area
+      findandapproachblock(); 
+      identifyblock();
+      liftblock();
+      returntoline();
+    } else if (pickup) {                                             // arrived outpost
+      release();
+    } else if ((blockNumber == 0 || blockNumber == 1) && !pickup) {  // arrived residential area                                                         
+      identifyblock();
+      liftblock();
+    }
   }
+
+  // while (1) {
+  //   stopmoving();
+  // }
 }
